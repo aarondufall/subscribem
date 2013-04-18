965bc332ec3d00d9086bbcf26ebcb072efc4fae5
Aaron Dufall <aald212@gmail.com>
2013-04-18 13:52:48 +0800

    User can only register one subdomain

--- spec/features/accounts/sign_up_spec.rb
+++ spec/features/accounts/sign_up_spec.rb
@@ -15,4 +15,18 @@ feature 'Accounts' do
 		page.should have_content('Signed in as subscribem@example.com')
 		page.current_url.should == "http://test.example.com/subscribem/"
 	end
+	scenario "cannot create and account with an already used subdomain" do
+		Subscribem::Account.create!(subdomain: 'test', name: 'test')
+		visit subscribem.root_path
+		click_link 'Account Sign Up'
+		fill_in 'Name', with: 'Test'
+		fill_in 'Subdomain', with: 'test'
+		fill_in 'Email', with: 'subscribem@example.com'
+		password_field_id = "account_owner_attributes_password"
+		fill_in password_field_id, with: 'password'
+		fill_in 'Password confirmation', with: 'password'
+		click_button 'Create Account'
+		page.should have_content("Sorry, your account could not be created.")
+		page.should have_content("Subdomain has already been taken")
+	end
 end
--- subscribem.gemspec
+++ subscribem.gemspec
@@ -23,4 +23,5 @@ Gem::Specification.new do |s|
   s.add_development_dependency "capybara", "2.0.2"
   s.add_dependency 'bcrypt-ruby', '3.0.1'
   s.add_dependency 'warden', '1.2.1'
+  s.add_dependency 'dynamic_form', '1.1.4'
 end
